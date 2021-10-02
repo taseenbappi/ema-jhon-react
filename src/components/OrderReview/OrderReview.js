2ed8bf5549506ab7f8aa98fbaa74d29f1c94aad4
IT Common <it-21@Globegroup.com>
2021-10-02 20:25:25 +0600

    product localdb

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
+import useProduct from '../../hooks/useProduct';
 
 const OrderReview = () => {
+    const [products] = useProduct();
+    const [cart] = useCart();
     return (
         <div>
             <h2>This is Order Review</h2>
+            <h1>Total Products: {products.length}</h1>
+            <p>use Card:{cart.length} </p>
         </div>
     );
 };
